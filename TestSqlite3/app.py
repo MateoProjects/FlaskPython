292475bfa28e2fb100d3fee907903952d5476c42
Ramon <mateo.trb@gmail.com>
2021-09-08 19:39:13 +0200

    Test Sqlite3 using python3

---
+++ TestSqlite3/app.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+
+# cur.execute('INSERT INTO users (FullName, Name, LastName, Age) VALUES ("RamonMateoNavarro", "Ramon", "Mateo", 26)')
+
+def insertFiles(values):
+    con = sqlite3.connect('C:\\Users\\fraud\\Desktop\\db\\mydb.db')
+    cur = con.cursor()
+    cur.execute('INSERT INTO dataAudio (NameFile, PathFile, DateFile, NumSentence, FullName) '
+                'VALUES (?, ?, ?, ?, ?)', values)
+
+    con.commit()
+    con.close()
+
+if __name__ == '__main__':
+    insertFiles(("Test1", "C:\\Users\\fraud\\Desktop\\csv.pdf", "01/01/98 23:59:59.999", 3, "RamonMateoNavarro"))
